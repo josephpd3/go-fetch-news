b7050f13ba86ac5bf1c92538efd00a39939f8151
Joseph DeBartola <josephdebartola@gmail.com>
2018-09-06 19:27:10 -0400

    Convert all tabs to spaces. Since tabs were the vscode default for Go, I though it was the standard. I was wrong.

--- refs/refs.go
+++ refs/refs.go
@@ -1,34 +1,34 @@
 package refs
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 
-	"github.com/PuerkitoBio/goquery"
+    "github.com/PuerkitoBio/goquery"
 )
 
 // Reference refers to a link within an article and its context (containing paragraph or sentence)
 type Reference struct {
-	Href    string
-	Text    string
-	Context string
+    Href    string
+    Text    string
+    Context string
 }
 
 func (r Reference) String() string {
-	return fmt.Sprintf("Reference{ Href: '%v', Text: '%v', Context: '%v'}", r.Href, r.Text, r.Context)
+    return fmt.Sprintf("Reference{ Href: '%v', Text: '%v', Context: '%v'}", r.Href, r.Text, r.Context)
 }
 
 // GQChannelAndCallback wraps an output channel and a callback for use in GoQuery selections
 type GQChannelAndCallback struct {
-	Channel  chan<- Reference
-	Callback func(int, *goquery.Selection, chan<- Reference)
+    Channel  chan<- Reference
+    Callback func(int, *goquery.Selection, chan<- Reference)
 }
 
 // CallWithRefChannel creates a function that injects an input channel implicitly via a closure
 func CallWithRefChannel(channelAndCallback GQChannelAndCallback) func(int, *goquery.Selection) {
-	return func(index int, element *goquery.Selection) {
-		channelAndCallback.Callback(index, element, channelAndCallback.Channel)
-	}
+    return func(index int, element *goquery.Selection) {
+        channelAndCallback.Callback(index, element, channelAndCallback.Channel)
+    }
 }
 
 // MergeOutboundReferenceChannels merges output channels of References
@@ -38,20 +38,20 @@ func CallWithRefChannel(channelAndCallback GQChannelAndCallback) func(int, *goqu
 // https://medium.com/justforfunc/two-ways-of-merging-n-channels-in-go-43c0b57cd1de
 // - - -
 func MergeOutboundReferenceChannels(cs ...<-chan Reference) <-chan Reference {
-	out := make(chan Reference)
-	var wg sync.WaitGroup
-	wg.Add(len(cs))
-	for _, c := range cs {
-		go func(c <-chan Reference) {
-			for v := range c {
-				out <- v
-			}
-			wg.Done()
-		}(c)
-	}
-	go func() {
-		wg.Wait()
-		close(out)
-	}()
-	return out
+    out := make(chan Reference)
+    var wg sync.WaitGroup
+    wg.Add(len(cs))
+    for _, c := range cs {
+        go func(c <-chan Reference) {
+            for v := range c {
+                out <- v
+            }
+            wg.Done()
+        }(c)
+    }
+    go func() {
+        wg.Wait()
+        close(out)
+    }()
+    return out
 }
