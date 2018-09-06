08af65d51616ea3b4db23737704d97957ca42f0f
Joseph P. DeBartola III <jdebartola@sonobi.com>
2018-09-06 16:07:28 -0400

    Create concurrent scrapers for NYT and WaPo * Set up structure for package * Write concurrent scraping functions for NYT and Washington * Post * Set up simple test with URL for each site

---
+++ publications/ny_times.go
@@ -0,0 +1,52 @@
+/*
+Package publications parses articles from various publications
+
+This file is specific to The New York Times
+*/
+package publications
+
+import (
+	"errors"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/josephpd3/gofetchnews/refs"
+)
+
+// FetchNYTReferences fetches all references in a New York Times article
+// If there are issues parsing the article's body, an error will be returned
+func FetchNYTReferences(article *goquery.Document) (<-chan refs.Reference, error) {
+	ch := make(chan refs.Reference)
+
+	articleBody := article.Find("article#story").First()
+	// Check to see if we have a match on our article body selector
+	if len(articleBody.Nodes) != 0 {
+		go parseNYTArticleBody(articleBody, ch)
+		return ch, nil
+	}
+
+	return nil, errors.New("could not parse article")
+}
+
+// Internal helper to parse paragraphs in article and delegate to parseNYTParagraph
+func parseNYTArticleBody(articleBody *goquery.Selection, ch chan<- refs.Reference) {
+	articleBody.Find("p").Each(refs.CallWithRefChannel(refs.GQChannelAndCallback{
+		Channel:  ch,
+		Callback: parseNYTParagraph,
+	}))
+	close(ch)
+}
+
+// Internal helper to parse links out of paragraphs to construct References to send back via channel
+func parseNYTParagraph(index int, element *goquery.Selection, ch chan<- refs.Reference) {
+	paragraphText := element.Text()
+	element.Find("a").Each(func(idx int, e *goquery.Selection) {
+		href, exists := e.Attr("href")
+		if exists {
+			ch <- refs.Reference{
+				Href:    href,
+				Text:    e.Text(),
+				Context: paragraphText,
+			}
+		}
+	})
+}
---
+++ refs/refs.go
@@ -0,0 +1,57 @@
+package refs
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// Reference refers to a link within an article and its context (containing paragraph or sentence)
+type Reference struct {
+	Href    string
+	Text    string
+	Context string
+}
+
+func (r Reference) String() string {
+	return fmt.Sprintf("Reference{ Href: '%v', Text: '%v', Context: '%v'}", r.Href, r.Text, r.Context)
+}
+
+// GQChannelAndCallback wraps an output channel and a callback for use in GoQuery selections
+type GQChannelAndCallback struct {
+	Channel  chan<- Reference
+	Callback func(int, *goquery.Selection, chan<- Reference)
+}
+
+// CallWithRefChannel creates a function that injects an input channel implicitly via a closure
+func CallWithRefChannel(channelAndCallback GQChannelAndCallback) func(int, *goquery.Selection) {
+	return func(index int, element *goquery.Selection) {
+		channelAndCallback.Callback(index, element, channelAndCallback.Channel)
+	}
+}
+
+// MergeOutboundReferenceChannels merges output channels of References
+// to create a single output channel of References
+// - - -
+// Courtesy of Francesc Campoy:
+// https://medium.com/justforfunc/two-ways-of-merging-n-channels-in-go-43c0b57cd1de
+// - - -
+func MergeOutboundReferenceChannels(cs ...<-chan Reference) <-chan Reference {
+	out := make(chan Reference)
+	var wg sync.WaitGroup
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go func(c <-chan Reference) {
+			for v := range c {
+				out <- v
+			}
+			wg.Done()
+		}(c)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
